f58b598f58af9a74338dacdffc20997eb268d544
Shivam jain <shivamjain228@gmail.com>
2020-11-05 18:37:44 +0530

    Add files via upload

---
+++ LEETCODE/November Daily Challenge/Minimum Cost to Move Chips to The Same Position.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int odd =0;
+        int even =0;
+        for(int i=0;i<position.size();i++)
+        {
+            if(position[i]%2==0)
+                even++;
+            else
+                odd++;      
+        }
+        if(even<odd)
+            return even;
+        else
+            return odd;
+    }
+};
